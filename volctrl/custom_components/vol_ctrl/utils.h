2aed1ef4f59433cf3c7b55c8b49a4e4ddc4ac785
Honza Slesinger <slesinger@gmail.com>
2025-07-05 10:37:03 +0200

    compile issue, api key

--- volctrl/custom_components/vol_ctrl/utils.h
+++ volctrl/custom_components/vol_ctrl/utils.h
@@ -2,18 +2,21 @@
 
 #include <string>
 
-namespace esphome {
-namespace vol_ctrl {
-namespace utils {
+namespace esphome
+{
+    namespace vol_ctrl
+    {
+        namespace utils
+        {
 
-// JSON parsing utilities
-bool extract_json_value(const std::string &json, const std::string &key, std::string &value);
-bool check_json_boolean(const std::string &response, const std::string &key, bool &result);
-bool extract_json_number(const std::string &response, const std::string &key, float &result);
+            // JSON parsing utilities
+            bool extract_json_value(const std::string &json, const std::string &key, std::string &value);
+            bool check_json_boolean(const std::string &response, const std::string &key, bool &result);
+            bool extract_json_number(const std::string &response, const std::string &key, float &result);
 
-// Date/time helper functions
-std::string get_datetime_string();
+            // Date/time helper functions
+            std::string get_datetime_string();
 
-}  // namespace utils
-}  // namespace vol_ctrl
-}  // namespace esphome
+        } // namespace utils
+    } // namespace vol_ctrl
+} // namespace esphome
